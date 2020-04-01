3e0ca46a8c9e13788585b7f8a9f57bedb0dd8660
deepti86026 <60600677+deepti86026@users.noreply.github.com>
2020-04-01 13:08:50 +0530

    Add files via upload

---
+++ registrationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace air_area.Areas.Customer.Controllers
+{
+    public class registrationController : Controller
+    {
+        // GET: Customer/registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Reg()
+        {
+            return View();
+        }
+    }
+}
